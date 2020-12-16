c575623e12661cf7c011ed7aa4bf2b6c3ad9e0d0
Ulrich Homann <ulho.microsoft@ulho.de>
2020-12-16 18:57:44 +0100

    actually do (sort of) dig

---
+++ Dns/Packet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Dns
+{
+    public class Packet
+    {
+        public Header header;
+        public Question question;
+        public Answer answer;
+    }
+
+    public class Header
+    {
+        // no c++ bitfield in C# and no pythcon pack -- todo: find a better way
+        public ushort id; // id to identify response (udp -> no connection, also a sort of security feature)
+        public bool qr; //query=0, response=1
+        public byte opcode; // actually only 4 bits
+        public bool aa; //authoritative answer
+        public bool tc; // truncation
+        public bool rd; // recursion desired
+        public bool ra; // recurion available
+        public byte z; // actually only 3 bits, reserved
+        public byte rcode; // actually only 4 bits,
+
+    }
+
+    public class Question
+    {
+
+    }
+
+    public class Answer
+    {
+
+    }
+}
--- dig/Program.cs
+++ dig/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Dns;
 
 namespace dig
 {
@@ -15,10 +18,63 @@ namespace dig
             // resolve server and request via system DNS (at least for server this is anyway required)
             System.Net.IPHostEntry serverHost = System.Net.Dns.GetHostEntry(server);
             System.Net.IPHostEntry requestHost = System.Net.Dns.GetHostEntry(request);
+            if (request[request.Length - 1] != '.')
+            {
+                request += '.';
+            }
 
-            Console.WriteLine("dig: server:" + serverHost.AddressList[0].ToString() + " request:" + requestHost.AddressList[0].ToString());
+            Console.WriteLine("dig: server: " + serverHost.AddressList[0].ToString() + " request: " + requestHost.AddressList[0].ToString());
 
+            // should use dns packet class, but now do it manually
+            int size = 0;
+            string[] labels = request.Split('.');
+            foreach (var label in labels)
+            {
+                size += label.Length + 1;
+                System.Console.WriteLine($"<{label}>");
+            }
 
+            byte[] raw = new byte[12 + size + 4];
+
+            raw[0] = 0; // id
+            raw[1] = 0;
+            raw[2] = 1; // flags
+            raw[3] = 0;
+            raw[4] = 0; // one query
+            raw[5] = 1;
+            raw[6] = 0; // no answer
+            raw[7] = 0;
+            raw[8] = 0; // no auth
+            raw[9] = 0;
+            raw[10] = 0; // no additional
+            raw[11] = 0;
+
+            int pos = 12;
+            foreach (var label in labels)
+            {
+                raw[pos++] = (byte)label.Length;
+                foreach (char c in label)
+                {
+                    raw[pos++] = (byte)c;
+                }
+            }
+            // the rfc is a bit misleading, no 16 bit padding
+            raw[pos++] = 0; // type a
+            raw[pos++] = 1;
+            raw[pos++] = 0; // class in
+            raw[pos++] = 1;
+
+            UdpClient client = new UdpClient();
+            int send = client.Send(raw, raw.Length, server, 53);
+            Console.WriteLine(" send bytes: " + send.ToString());
+
+            IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+            raw = client.Receive(ref remote);
+
+            foreach (var bt in raw)
+            {
+                Console.WriteLine(bt.ToString());
+            }
         }
     }
 }

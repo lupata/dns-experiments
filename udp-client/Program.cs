73d8945c2a7350d31600afbf0cd7cad1fdb0bec3
Ulrich Homann <ulho.microsoft@ulho.de>
2020-12-18 17:48:24 +0100

    work in progress: don't know to handle lost UDP packets, the UdpClient gets resets if done in parallel

--- udp-client/Program.cs
+++ udp-client/Program.cs
@@ -2,28 +2,59 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace udp_client
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
             string server = args[0];
-            string message = args[1];
+            int count = int.Parse(args[1]);
+
+            static void Receive(IAsyncResult ar)
+            {
+                try {
+                    UdpClient client = (UdpClient)ar.AsyncState;
+                    IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] raw = Encoding.ASCII.GetBytes("connection error");
+                    try
+                    {
+                        raw = client.EndReceive(ar, ref remote);
+                    } catch (Exception e) {
+                        // it might be that ICMP messages are generated, ignoe it
+                        Console.WriteLine(e.ToString());
+                    }
+                    client.BeginReceive(new AsyncCallback(Receive), client);
+
+                    string received = Encoding.ASCII.GetString(raw);
+                    Console.WriteLine(" received: " + received);
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.ToString());
+                }
+            }
 
             try {
-                UdpClient client = new UdpClient();
-                Byte[] raw = Encoding.ASCII.GetBytes(message);
-                int send = client.Send(raw, raw.Length, server, 6653);
-                Console.WriteLine(" send bytes: " + send.ToString());
+                UdpClient client = new UdpClient(0); // an appropriate local port number is assigned
 
-                IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
-                raw = client.Receive(ref remote);
-                string received = Encoding.ASCII.GetString(raw);
-                Console.WriteLine(" received: " + received);
-                Console.WriteLine(" from: " + remote.Address.ToString() + ":" + remote.Port.ToString());
+                // client.BeginReceive(new AsyncCallback(Receive), client);
 
+                for (int i=0; i<count; ++i) {
+                    Byte[] raw = Encoding.ASCII.GetBytes(i.ToString());
+                    client.Send(raw, raw.Length, server, 6653);
+
+                    IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+                    raw = client.Receive(ref remote);
+  
+                    string received = Encoding.ASCII.GetString(raw);
+                    Console.WriteLine(" received: " + received);
+                }
+
+                // Thread.Sleep(1000);
+                // Console.ReadKey();
                 client.Close();
 
             } catch (Exception e) {
